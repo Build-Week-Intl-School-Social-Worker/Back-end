baee68449ddcecfa938825dbcb680ca1ea719bc7
Jess Daniel <jessdaniel474@gmail.com>
2020-02-03 19:36:43 -0500

    added root route message

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const server = require('./api/server');
 
 const PORT = process.env.PORT || 5000;
 
+server.get("/", (req, res) => {
+    res.status(200).json({ message: "I'm alive and well" });
+})
+
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 })              
